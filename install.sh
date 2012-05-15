7cb8aa0858b6168fd0805a60b279806a5a43d94c
Jonathan Saggau <saggau@gmail.com>
2012-05-15 01:08:58 -0700

    latest Xcode snippets; minor edits to install and readme

--- install.sh
+++ install.sh
@@ -6,12 +6,17 @@ BASEDIR=$(cd "$(dirname "$0")"; pwd)
 #backup existing user code snippets
 mkdir -p ~/Library/Developer/Xcode/UserData/CodeSnippets.bak
 cp -r ~/Library/Developer/Xcode/UserData/CodeSnippets/* ~/Library/Developer/Xcode/UserData/CodeSnippets.bak/
+
+#rm existing user code snippets
 rm -r ~/Library/Developer/Xcode/UserData/CodeSnippets/*
 
-#remove the system code snippets
-sudo rm /Applications/Xcode.app/Contents/PlugIns/IDECodeSnippetLibrary.ideplugin/Contents/Resources/SystemCodeSnippets.codesnippets 
-sudo touch /Applications/Xcode.app/Contents/PlugIns/IDECodeSnippetLibrary.ideplugin/Contents/Resources/SystemCodeSnippets.codesnippets
+#uncomment the below to remove the system code snippets
+#sudo rm /Applications/Xcode.app/Contents/PlugIns/IDECodeSnippetLibrary.ideplugin/Contents/Resources/SystemCodeSnippets.codesnippets 
+#sudo touch /Applications/Xcode.app/Contents/PlugIns/IDECodeSnippetLibrary.ideplugin/Contents/Resources/SystemCodeSnippets.codesnippets
 
 #convert and uncrustify the snippets
+
 python "$BASEDIR"/convertSnippets.py  "$BASEDIR"/snippets "$BASEDIR"/uncrustifyTemplates/sbi.cfg
 cp -v "$BASEDIR"/snippets/pythonOut/*.codesnippet ~/Library/Developer/Xcode/UserData/CodeSnippets/  
+
+echo "move any snippets in ~/Library/Developer/Xcode/UserData/CodeSnippets.bak that you would like to keep into ~/Library/Developer/Xcode/UserData/CodeSnippets/"
